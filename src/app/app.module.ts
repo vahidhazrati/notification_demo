9f75276501cc5c0285698c3aa01fd07ac8b4584b
vahidhazrati <vahid.h63@hotmail.com>
2022-08-23 09:46:05 +0800

    oopoo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from "../environments/environment";
 import { AppComponent } from './app.component';
-
+import { initializeApp } from "firebase/app";
+initializeApp(environment.firebase);
 @NgModule({
   declarations: [
     AppComponent
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCb4h7rXaWDmb3jJ1dc2mAXzzT18lmYPaM",
+    authDomain: "notificationdemo-89ab4.firebaseapp.com",
+    projectId: "notificationdemo-89ab4",
+    storageBucket: "notificationdemo-89ab4.appspot.com",
+    messagingSenderId: "688240627339",
+    appId: "1:688240627339:web:72299e2b583e5f1ab50e8e",
+    measurementId: "G-B9339TYQY6",
+    vapidKey: "BM9C18PDt1g6_khAX97s2fFIAKzCOfoXuoqIcVJcEnbfseNhy0RwgkWHu2CZvDstcxoUG0Vp8244dOVh1GO_4S8"
+  },
 };
 
 /*
